97eea19cc891d45f9fe402d0b46127d54888acc6
Claire O'Brien <186298887+Claire135@users.noreply.github.com>
2025-01-06 20:20:28 +0100

    grid box is working with any number

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 console.log("Hello World")
 
-input = parseInt(prompt("Enter number of boxes"))
+input = parseInt(prompt("Enter number of boxes"), 10)
 
-let size = 500/input;
+let size = (500/input)*10/10;
 
 const grid = document.querySelector('#grid');
-const gridBox = document.createElement('div');
-gridBox.style.height = `${size}px`;
-gridBox.style.width = `${size}px`;
-gridBox.style.boxSizing = "border-box";
-grid.appendChild(gridBox)
+grid.innerHTML = '';
 
+console.log("Calculated size for each box:", size);
 
-/*function generateGrid (input, size){    
-//    for (i=0; i<input; i++){
- //       const box = generateBox(size); //append result to DOM
-  //      grid.appendChild(box);
-   // }
-//}
+const noOfBoxes = input*input;
 
-const genGridBtn = document.querySelector("#genGridBtn");
-genGridBtn.addEventListener("click", () => {
-    generateBox(size)
-});*/
+console.log("No of boxes:", noOfBoxes);
+
+for (let i=0; i<noOfBoxes; i++){
+    console.log(`Creating box #${i + 1}`);
+    const gridBox = document.createElement('div');
+    gridBox.style.height = `${size}px`;
+    gridBox.style.width = `${size}px`;
+    gridBox.style.background  =" #f8f8f8"
+    gridBox.style.border  ="2px solid purple"
+    gridBox.style.margin = "0px"
+    gridBox.style.padding = "0px"
+    gridBox.style.boxSizing= "border-box"
+    grid.appendChild(gridBox)
+}
 
 
 
