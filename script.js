741e8dbdac8c53fdb2c20fed84fd548bab9e533e
Claire O'Brien <186298887+Claire135@users.noreply.github.com>
2025-01-06 18:42:30 +0100

    can generate 1 box based on number of boxes to be generated

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 console.log("Hello World")
 
-function generateGrid (input){
-    let row = "";
-    for (i=0; i<=input; i++){
-        row+= "*"
-    }
-    return row;
-}
-console.log(generateGrid(27));
+input = parseInt(prompt("Enter number of boxes"))
+
+let size = 500/input;
+
+const grid = document.querySelector('#grid');
+const gridBox = document.createElement('div');
+gridBox.style.height = `${size}px`;
+gridBox.style.width = `${size}px`;
+gridBox.style.boxSizing = "border-box";
+grid.appendChild(gridBox)
 
 
+/*function generateGrid (input, size){    
+//    for (i=0; i<input; i++){
+ //       const box = generateBox(size); //append result to DOM
+  //      grid.appendChild(box);
+   // }
+//}
 
-
-
-
-
-
+const genGridBtn = document.querySelector("#genGridBtn");
+genGridBtn.addEventListener("click", () => {
+    generateBox(size)
+});*/
 
 
 
