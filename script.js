5f924594b958769cf036ce0373f7b97320ed325c
Claire O'Brien <186298887+Claire135@users.noreply.github.com>
2025-01-06 14:33:35 +0100

    logic for creating one row written

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("Hello World")
+
+function generateGrid (input){
+    let row = "";
+    for (i=0; i<=input; i++){
+        row+= "*"
+    }
+    return row;
+}
+console.log(generateGrid(27));
+
+
+
+
+
+
+
+
+
+
+
+
+
